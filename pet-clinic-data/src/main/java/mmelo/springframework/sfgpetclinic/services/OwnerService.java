58c7b1d8798c0197b485603506538b140ac28e36
Magnaldo Melo <magnaldo.melo@gmail.com>
2021-04-02 18:21:29 -0300

    Adding crud service

--- pet-clinic-data/src/main/java/mmelo/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/mmelo/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,12 +2,7 @@ package mmelo.springframework.sfgpetclinic.services;
 
 import mmelo.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/mmelo/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/mmelo/springframework/sfgpetclinic/services/PetService.java
@@ -2,11 +2,5 @@ package mmelo.springframework.sfgpetclinic.services;
 
 import mmelo.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
